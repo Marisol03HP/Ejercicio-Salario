aa9c791f37d56bb64ea0913baf1d5f0c0e0f76d1
unknown <hurtadoperezmarisol03@gmail.com>
2025-02-07 20:40:47 -0600

    Agregue un script

---
+++ Salario.js
@@ -0,0 +1,7 @@
+const costoPorHora = 15;
+const horasTrabajadas = 40;
+
+const salarioSemanal = costoPorHora * horasTrabajadas;
+
+console.log(`Horas trabajadas: ${horasTrabajadas}`);
+console.log(`Salario semanal: $${salarioSemanal}`);
